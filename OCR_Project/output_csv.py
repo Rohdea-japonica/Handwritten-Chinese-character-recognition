150eaede07a2a3877aff524a28a7220d5660ff09
Sijia-Shen <1473700558@qq.com>
2024-05-24 17:49:44 +0800

    1.1.3 无题，结题

--- OCR_Project/output_csv.py
+++ OCR_Project/output_csv.py
@@ -2,6 +2,7 @@ import torch
 import pandas as pd
 from torch.optim import optimizer
 from torch.utils.data import DataLoader
+
 from OCR_Project import OCR_Model, MyDataset
 
 if torch.cuda.is_available():  # 训练处理器
@@ -13,7 +14,7 @@ module = input("请输入训练模式：")
 
 # 获取训练数据集
 train_dataset = MyDataset()
-train_dataset.getdata("../data", module)   # 注意，此处使用test模式，需要在data文件夹下有一个test文件夹用于存放图片数据
+train_dataset.getdata("../data", module)  # 注意，此处使用test模式，需要在data文件夹下有一个test文件夹用于存放图片数据
 train_loader = DataLoader(train_dataset, batch_size, drop_last=False)
 
 # 加载模型并用于预测
@@ -28,5 +29,7 @@ for x, y in train_loader:
     pred = model(x.to(device))
     label = pred.argmax(1)
     labels = labels + label.tolist()
-df_labels = pd.DataFrame(labels)
-df_labels.to_csv(module + "_dict.csv", mode='a', header=False, index=None)
+
+df = pd.read_csv(module + "_dict.csv")
+df["label"] = labels
+df.to_csv(module + "_dict.csv", index=None)
